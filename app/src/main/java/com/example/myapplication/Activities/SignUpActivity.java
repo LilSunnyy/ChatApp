fafc2735e61da4c687f138da6a0d424c91e683e1
LilSunnyy <chickenattack6969@gmail.com>
2023-07-09 20:20:56 +0700

    1st commit

---
+++ app/src/main/java/com/example/myapplication/Activities/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivitySignInBinding;
+import com.example.myapplication.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners() {
+        binding.txtSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
